b7dad79dd3a3fb69c49a6d72e46df735c9339202
anhquan0412 <anhquan0412@gmail.com>
2016-04-04 02:06:05 -0500

    adding 2 AI classes

---
+++ src/tictactoeai/Level2AI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoeai;
+
+/**
+ *
+ * @author anhqu
+ */
+public class Level2AI extends Level1AI {
+
+    public Level2AI(Board b) {
+        super(b);
+    }
+
+    @Override
+    public int[] move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
